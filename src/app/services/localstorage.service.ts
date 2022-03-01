2c626ce0740a3d573277093dc19723200188b4ff
samahrefaat <samahrefaat1@gmail.com>
2022-03-01 12:15:05 +0200

    nnnn

---
+++ src/app/services/localstorage.service.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Injectable } from '@angular/core';
+
+const TOKEN = 'jwtToken';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalstorageService {
+  setToken(data: any) {
+    localStorage.setItem(TOKEN, data);
+  }
+
+  getToken(): any {
+    return localStorage.getItem(TOKEN);
+  }
+
+  removeToken() {
+    localStorage.removeItem(TOKEN);
+  }
+}
